18d31f6ec1ff4fa0e4b08dc3899f340fe3dda645
axel nolasco <axlnolasco@gmail.com>
2024-12-17 16:31:17 -0600

    feat: setup github workspace

--- locals.tf
+++ locals.tf
@@ -10,7 +10,13 @@ locals {
         description = "Example description of workspace"
         execution_mode = "remote"
         project_id = module.project["axels-eci-project"].id
-        vcs_repo_identifier = "axelNolasco/fem-eci-terraform-tfe"
+        vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-tfe"
+    }
+    "axels-eci-github" = {
+        description = "Example automation workspace for GitHub resources"
+        execution_mode = "local"
+        project_id = module.project["axels-eci-project"].id
+        vcs_repo_identifier = "${var.github_organization_name}/fem-eci-terraform-github"
     }
   }
 }
